d130a1c4bf7b964d4d8d97a51138c5017a92fae3
ben-schmid <114110262+ben-schmid@users.noreply.github.com>
2024-05-06 20:05:35 -0400

    started on frontend page UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect} from 'react'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
 
 
 //https://mui.com/.hr
@@ -30,14 +36,41 @@ function App() {
   }
 
   return (
-    <div>
-      {message ? (
-        <p>{format_message(message)}</p> 
-      ) : (
-        <p>Loading...</p>
-      )}
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      justifyContent: 'flex-end', 
+      alignItems: 'center', 
+      height: '100vh' }}>
+      <div>
+        {message ? (
+          <p>{format_message(message)}</p> 
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+      <Box
+      sx={{
+        width: 1000,
+        maxWidth: '100%',
+        mb: 4
+      }}
+    >
+      <Stack direction="row" spacing = {2}>
+      <TextField 
+      fullWidth label="Message" 
+      id="fullWidth" 
+      />
+      <Button variant="contained" endIcon={<SendIcon />}>
+        Send
+      </Button>
+      </Stack>
+    </Box>
+  
+      
     </div>
   );
+  
 }
 
 
