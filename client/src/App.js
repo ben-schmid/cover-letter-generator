7cc717f8f0c57ff5ccccebf0d02070cf29b9d2d9
Ben Schmid <ben.schmid1@gmail.com>
2024-05-26 19:05:42 -0400

    worked on css styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
@@ -10,89 +10,93 @@ import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
+const format_message = (text) => {
+  return text.split('\n').map((line, index) => (
+    <span key={index}>
+      {line}
+      <br />
+    </span>
+  ));
+}
 
-//https://mui.com/.hr
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 
+function uploadButton() {
+  return (
+    <Button
+      component="label"
+      role={undefined}
+      variant="contained"
+      tabIndex={-1}
+      startIcon={<CloudUploadIcon />}
+    >
+      Upload file
+      <VisuallyHiddenInput type="file" />
+    </Button>
+  );
+}
 
-function App() {
-  const [message, setMessage] = useState ("")
+export default function App() {
+  const [message, setMessage] = useState("");
 
-  useEffect (() => {
-    console.log("Fetching...")
+  useEffect(() => {
+    console.log("Fetching...");
     fetch('/chat').then(
       res => res.json()
     ).then(
       data => {
-        setMessage(data.message)
-        console.log(data)
-      })
-  }, [])
-
-  const format_message = (text) => {
-    return text.split('\n').map((line, index) => (
-      <span key={index}>
-        {line}
-        <br />
-        </span>
-    ))
-  
-  }
-  const VisuallyHiddenInput = styled('input')({
-    clip: 'rect(0 0 0 0)',
-    clipPath: 'inset(50%)',
-    height: 1,
-    overflow: 'hidden',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    whiteSpace: 'nowrap',
-    width: 1,
-  });
+        setMessage(data.message);
+        console.log(data);
+      });
+  }, []);
 
   return (
-    <div style={{ 
-      marginTop: 8,
-      display: 'flex', 
-      flexDirection: 'column', 
-      alignItems: 'center' }}>
-      <div>
-        {message ? (
-          <p>{format_message(message)}</p> 
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-      <Box
-        sx={{
-          width: 1000,
-          maxWidth: '100%',
-          mb: 4
-        }}
-        >
-        <Stack direction="row" spacing = {2}>
-          <TextField 
-            fullWidth label="Message" 
-            id="fullWidth" 
-          />
-          <button
-            component="label"
-            role={undefined}
-            variant="contained"
-            tabIndex={-1}
-            startIcon={<CloudUploadIcon />}
+    <div className="app">
+      <section className="sidebar">
+        <Button variant="contained">New Chat</Button>
+      </section>
+
+      <section className="main">
+        <div className="message-container">
+          {message ? (
+            <p>{format_message(message)}</p>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+
+        <section className="bottom-section">
+          <Box
+            sx={{
+              width: 1000,
+              maxWidth: '100%',
+              color: 'white',
+              mb: 4
+            }}
           >
-          Upload file
-            <VisuallyHiddenInput type="file" />
-          </button>
-          <Button variant="contained" endIcon={<SendIcon />}>
-            Send
-          </Button>
-        </Stack>
-      </Box>  
+            <Stack direction="row" spacing={2}>
+              <TextField
+                fullWidth label="Message"
+                id="fullWidth"
+                fontColor="white"
+              />
+              <Button variant="contained" endIcon={<SendIcon />}>
+                Send
+              </Button>
+            </Stack>
+          </Box>
+        </section>
+      </section>
     </div>
   );
-  
 }
-
-
-export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
